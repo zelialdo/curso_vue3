be79f65791f774e6562f0ad5c133d4685ab6ecea
Zelialdo Sueldo <71799490+zelialdo@users.noreply.github.com>
2023-04-05 10:25:06 -0300

    v-if v-show

--- vue-project/src/components/Info.vue
+++ vue-project/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de emprego</p>
     <p>Utilizo as seguintes tecnologias</p>
     <ul>
       <li>JavaScript</li>
       <li>PHP</li>
       <li>Python</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+      return {
+        esta_trabalhando: false,
+        mostrar_email: true,
+        email: 'zs@email.com'
+      }
+    }
 }
 </script>
