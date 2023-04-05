06498160d33e2ec0eb96a207cb6d9af9c1583f1a
Zelialdo Sueldo <71799490+zelialdo@users.noreply.github.com>
2023-04-05 09:02:28 -0300

    life Cycle

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,14 +1,19 @@
 <template>
- <PrimeiroComponente />
+  <div>
+    <PrimeiroComponente />
+    <LifeCycle/>
+  </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/icons/PrimeiroComponente.vue'
+  import LifeCycle from './components/icons/LifeCycle.vue'
 
 export default {
   name: 'App',
   components: {
-    PrimeiroComponente
+    PrimeiroComponente,
+    LifeCycle
   }
 }
 </script>
---
+++ vue-project/src/components/icons/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>
+    Meu nome é: {{ nome }}
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'LifeCycle',
+  data(){
+    return{
+      nome: 'Ainda não sei'
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.nome = "Zé"
+    }, 1000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "Pedro"
+    }, 2000);
+  }
+}
+</script>
