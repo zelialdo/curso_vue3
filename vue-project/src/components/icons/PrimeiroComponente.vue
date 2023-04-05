b66ce2c224345ea2aad5a7f47899157efe158d00
Zelialdo Sueldo <71799490+zelialdo@users.noreply.github.com>
2023-04-05 08:32:24 -0300

    melhorando componente

--- vue-project/src/components/icons/PrimeiroComponente.vue
+++ vue-project/src/components/icons/PrimeiroComponente.vue
@@ -1,11 +1,20 @@
 <template>
+  <div>
     <h1>
-        Ola vue!
+      Ola vue!
     </h1>
+    <p>Meu Nome é {{ nome }} e trablho como {{ profissao }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'PrimeiroComponente'
+  name: 'PrimeiroComponente',
+  data() {
+    return{
+      nome: "Zelialdo",
+      profissao: "Programador"
+    }
+  }
 }
 </script>
